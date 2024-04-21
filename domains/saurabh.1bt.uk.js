b7215bbf7b721c343bd35f22d22bd8e44ab42941
Saurabh <61317144+0xSaurabhx@users.noreply.github.com>
2024-04-21 18:35:55 +0530

    Create saurabh.1bt.uk.js

---
+++ domains/saurabh.1bt.uk.js
@@ -0,0 +1,15 @@
+addSubDomain({
+  description: 'Welcome to my portfolio!',
+  domain: '1bt.uk',
+  subdomain: 'saurabh',
+  owner: {
+    email: 'dev.saurabhm@gmail.com',
+  },
+  record: {
+    TXT: [{
+      name: "_vercel",
+      value: "vc-domain-verify=saurabh.1bt.uk,4ce827037647847d28fa",
+    }],
+  },
+  proxy: false
+})
