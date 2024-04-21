ad03b834098e49ae3788d7178a5a0d5611a76e6c
Saurabh <61317144+0xSaurabhx@users.noreply.github.com>
2024-04-21 18:32:48 +0530

    Create saurabh.1bt.uk.js

---
+++ domains/saurabh.1bt.uk.js
@@ -0,0 +1,15 @@
+addSubDomain({
+  description: 'A free hosting website with a lot of features and good uptime',
+  domain: '1bt.uk',
+  subdomain: 'saurabh',
+  owner: {
+    email: 'dev.saurabhm@gmail.com',
+  },
+  record: {
+    TXT: [{
+      name: "_vercel",
+      value: "vc-domain-verify=saurabh.1bt.uk,4ce827037647847d28fa",
+    }],
+  },
+  proxy: false
+})
